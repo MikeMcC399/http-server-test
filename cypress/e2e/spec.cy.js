44c97eb1dec8e96e0f0d0d65c0d18c10705507ea
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2023-02-25 16:59:39 +0100

    Add test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,29 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe.skip('hostname test via cy.request', () => {
+
+  it('localhost', () => {
+    cy.request('http://localhost:3000');
+  });
+
+  it('loopback IPv4', () => {
+    cy.request('http://127.0.0.1:3000');
+  });
+
+  it('loopback IPv6', () => {
+    cy.request('http://[::1]:3000');
+  });
+});
+
+describe('hostname test via cy.visit', () => {
+
+  it('localhost', () => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('loopback IPv4', () => {
+    cy.visit('http://127.0.0.1:3000');
+  });
+
+  it.only('loopback IPv6', () => {
+    cy.visit('http://[::1]:3000');
+  });
+});
